155068f94aeb740f760f176611f7974f774953d3
88chinu <chinmaya.pa42@gmail.com>
2024-12-22 18:27:23 +0000

    prod deploy:update navebar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,37 +1,29 @@
 import React, { useContext, useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom'; // Add this
+import { Link as RouterLink } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
   Typography,
-  Button,
-  Box,
   IconButton,
   Menu,
   MenuItem,
+  Box,
 } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import HomeIcon from '@mui/icons-material/Home';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import MenuIcon from '@mui/icons-material/Menu';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { ThemeModeContext } from './ThemeModeProvider'; // Ensure correct path
 
-const notesPages = [
-  { title: 'Home', path: '/notes/home' },
-  { title: 'Schedule', path: '/notes/schedule' },
-];
-
 const Navbar = () => {
-  const [notesAnchorEl, setNotesAnchorEl] = useState(null);
+  const [menuAnchorEl, setMenuAnchorEl] = useState(null);
   const { mode, toggleTheme } = useContext(ThemeModeContext);
 
-  const handleNotesClick = (event) => {
-    setNotesAnchorEl(event.currentTarget);
+  const handleMenuOpen = (event) => {
+    setMenuAnchorEl(event.currentTarget);
   };
 
-  const handleNotesClose = () => {
-    setNotesAnchorEl(null);
+  const handleMenuClose = () => {
+    setMenuAnchorEl(null);
   };
 
   return (
@@ -39,68 +31,54 @@ const Navbar = () => {
       <Toolbar>
         <Typography
           variant="h6"
-          component="div"
-          sx={{ flexGrow: 1, color: 'text.primary', fontFamily: 'Roboto Slab, serif' }}
+          component={RouterLink}
+          to="/"
+          sx={{
+            flexGrow: 1,
+            color: 'text.primary',
+            textDecoration: 'none',
+            fontFamily: 'Roboto Slab, serif',
+          }}
         >
           Clinic Management
         </Typography>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          <Button
-            color="inherit"
-            component={RouterLink}
-            to="/"
-            startIcon={<HomeIcon />}
-            sx={{
-              textTransform: 'none',
-              color: 'text.primary',
-              fontFamily: 'Lato, sans-serif',
-            }}
-          >
-            Home
-          </Button>
-          <Button
-            color="inherit"
-            onClick={handleNotesClick}
-            startIcon={<MenuBookIcon />}
-            sx={{
-              textTransform: 'none',
-              color: 'text.primary',
-              fontFamily: 'Lato, sans-serif',
-            }}
-          >
-            Notes
-          </Button>
-          <Menu
-            anchorEl={notesAnchorEl}
-            open={Boolean(notesAnchorEl)}
-            onClose={handleNotesClose}
-          >
-            {notesPages.map((page) => (
-              <MenuItem
-                key={page.path}
-                component={RouterLink}
-                to={page.path}
-                onClick={handleNotesClose}
-                sx={{ color: 'text.primary', fontFamily: 'Lato, sans-serif' }}
-              >
-                {page.title}
-              </MenuItem>
-            ))}
-          </Menu>
-          <IconButton
-            component="a"
-            href="https://github.com/88chinu/clinicManagement"
-            target="_blank"
-            rel="noopener noreferrer"
-            sx={{
-              color: 'text.primary',
-            }}
-          >
-            <GitHubIcon />
-          </IconButton>
-          <IconButton onClick={toggleTheme} color="inherit">
+        <IconButton onClick={toggleTheme} color="inherit">
             {mode === 'light' ? <Brightness4Icon /> : <Brightness7Icon />}
           </IconButton>
+          <IconButton onClick={handleMenuOpen} color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={menuAnchorEl}
+            open={Boolean(menuAnchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem
+              component={RouterLink}
+              to="/add"
+              onClick={handleMenuClose}
+              sx={{ color: 'text.primary', fontFamily: 'Lato, sans-serif' }}
+            >
+              Create
+            </MenuItem>
+            {/* <MenuItem
+              component={RouterLink}
+              to="/edit/:id"
+              onClick={handleMenuClose}
+              sx={{ color: 'text.primary', fontFamily: 'Lato, sans-serif' }}
+            >
+              Update Patient
+            </MenuItem> */}
+            <MenuItem
+              component={RouterLink}
+              to="/About"
+              onClick={handleMenuClose}
+              sx={{ color: 'text.primary', fontFamily: 'Lato, sans-serif' }}
+            >
+              About
+            </MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
