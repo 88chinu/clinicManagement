60b481872a470cccda21b4c56dfb6039bd4a8057
88chinu <chinmaya.pa42@gmail.com>
2025-01-03 15:47:48 +0000

    prod deploy:update theme

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { ThemeModeProvider } from './components/ThemeModeProvider';
 import { CssBaseline, Box, Container } from '@mui/material';
-import { SnackbarProvider } from 'notistack'; // Import SnackbarProvider
-import { createThemeByMode } from './container/Theme'; // Corrected import statement
+import { SnackbarProvider } from 'notistack';
 
 import HomePage from './components/HomePage';
 import SearchPage from './components/SearchPatient';
@@ -15,40 +14,54 @@ import PatientEdit from './components/UpdatePatient';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import ExportPage from './components/ExportPage';
+import ThemeSwitcher from './components/ThemeSwitcher';
 
-const App = () => {
-    return (
-        <ThemeModeProvider theme={createThemeByMode('light')}>  {/* Set your mode here */}
-            <CssBaseline />
-            {/* Wrap your application with SnackbarProvider */}
-            <SnackbarProvider
-                maxSnack={3} // Maximum number of notifications displayed simultaneously
-                anchorOrigin={{
-                    vertical: 'top', // Notifications will appear at the bottom
-                    horizontal: 'left', // Notifications aligned to the left
-                }}
-            >
-                <Router>
-                    <Box display="flex" flexDirection="column" minHeight="100vh">
-                        <Navbar />
-                        <Container component="main" flex="1" className="box-container">
-                            <Routes>
-                                <Route exact path="/" element={<HomePage />} />
-                                <Route path="/list" element={<PatientList />} />
-                                <Route path="/add" element={<PatientAdd />} />
-                                <Route path="/edit/:id" element={<PatientEdit />} />
-                                <Route path="/detail/:id" element={<PatientDetail />} />
-                                <Route path="/search" element={<SearchPage />} />
-                                <Route path="/export" element={<ExportPage />} />
-                                <Route path="*" element={<div>404 - Page Not Found</div>} />
-                            </Routes>
-                        </Container>
-                        <Footer />
+const AppContent = () => (
+    <>
+        <CssBaseline />
+        <SnackbarProvider
+            maxSnack={3}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+        >
+            <Router>
+                <Box display="flex" flexDirection="column" minHeight="100vh">
+                    <Navbar />
+                    <Box
+                        display="flex"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        px={2}
+                        py={1}
+                    >
+                        <h1 style={{ margin: 0 }}>Patient Management</h1>
+                        <ThemeSwitcher />
                     </Box>
-                </Router>
-            </SnackbarProvider>
-        </ThemeModeProvider>
-    );
-};
+                    <Container component="main" flex="1" className="box-container">
+                        <Routes>
+                            <Route exact path="/" element={<HomePage />} />
+                            <Route path="/list" element={<PatientList />} />
+                            <Route path="/add" element={<PatientAdd />} />
+                            <Route path="/edit/:id" element={<PatientEdit />} />
+                            <Route path="/detail/:id" element={<PatientDetail />} />
+                            <Route path="/search" element={<SearchPage />} />
+                            <Route path="/export" element={<ExportPage />} />
+                            <Route path="*" element={<div>404 - Page Not Found</div>} />
+                        </Routes>
+                    </Container>
+                    <Footer />
+                </Box>
+            </Router>
+        </SnackbarProvider>
+    </>
+);
+
+const App = () => (
+    <ThemeModeProvider>
+        <AppContent />
+    </ThemeModeProvider>
+);
 
 export default App;
